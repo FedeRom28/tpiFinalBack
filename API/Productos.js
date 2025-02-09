d43f94ff8645ab6bacd0250bb7592e6721fd2b19
BauduccoT <165963762+jjoaaco@users.noreply.github.com>
2025-02-09 17:50:28 -0300

    arreglos stock

--- API/Productos.js
+++ API/Productos.js
@@ -7,16 +7,35 @@ const upload = require('../config/uploadConfig'); // Importar la configuración 
 router.post("/", upload.single('imagen'), function (req, res, next) {
     const { nom_producto, descripcion, precio, id_categorias, id_talles, cantidad } = req.body;
     const imagen = req.file ? req.file.filename : null;
-    const sqlProducto = "INSERT INTO productos (nom_producto, descripcion, precio, id_categorias, imagen, id_talles, cantidad) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    const valoresProducto = [nom_producto, descripcion, precio, id_categorias, imagen, id_talles, cantidad];
+
+    console.log(req.file, imagen);
     
-    conexion.query(sqlProducto, valoresProducto, function (error, result) {
-        if (error) {
-            console.error(error);
-            return res.status(500).send("Ocurrió un error al agregar el producto");
-        }
-        return res.json({ status: "ok", message: "Producto agregado con éxito" });
-    });
+
+    if(imagen !== null){
+        const sqlProducto = "INSERT INTO productos (nom_producto, descripcion, precio, id_categorias, imagen, id_talles, cantidad) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        const valoresProducto = [nom_producto, descripcion, precio, id_categorias, imagen, id_talles, cantidad];
+
+        conexion.query(sqlProducto, valoresProducto, function (error, result) {
+            if (error) {
+                console.error(error);
+                return res.status(500).send("Ocurrió un error al agregar el producto");
+            }
+            return res.json({ status: "ok", message: "Producto agregado con éxito" });
+        });
+    }
+    else{
+        const sqlProducto = "INSERT INTO productos (nom_producto, descripcion, precio, id_categorias, id_talles, cantidad) VALUES (?, ?, ?, ?, ?, ?)";
+        const valoresProducto = [nom_producto, descripcion, precio, id_categorias, id_talles, cantidad];
+
+        conexion.query(sqlProducto, valoresProducto, function (error, result) {
+            if (error) {
+                console.error(error);
+                return res.status(500).send("Ocurrió un error al agregar el producto");
+            }
+            return res.json({ status: "ok", message: "Producto agregado con éxito" });
+        });
+    }
+    
 });
 
 // Actualizar un producto por ID con imagen, talle y cantidad
@@ -24,16 +43,41 @@ router.put("/:id", upload.single('imagen'), function (req, res, next) {
     const { id } = req.params;
     const { nom_producto, descripcion, precio, id_categorias, id_talles, cantidad } = req.body;
     const imagen = req.file ? req.file.filename : null;
-    const sqlProducto = "UPDATE productos SET nom_producto = ?, descripcion = ?, precio = ?, id_categorias = ?, imagen = ?, id_talles = ?, cantidad = ? WHERE id_productos = ?";
-    const valoresProducto = [nom_producto, descripcion, precio, id_categorias, imagen, id_talles, cantidad, id];
-    
-    conexion.query(sqlProducto, valoresProducto, function (error) {
-        if (error) {
-            console.error(error);
-            return res.status(500).send("Ocurrió un error al actualizar el producto");
-        }
-        return res.json({ status: "ok", message: "Producto actualizado con éxito" });
-    });
+
+    console.log(req.file, imagen);
+
+    if(imagen !== null){
+        console.log("entro a imagen");
+        
+        const sqlProducto = "UPDATE productos SET nom_producto = ?, descripcion = ?, precio = ?, id_categorias = ?, imagen = ?, id_talles = ?, cantidad = ? WHERE id_productos = ?";
+        const valoresProducto = [nom_producto, descripcion, precio, id_categorias, imagen, id_talles, cantidad, id];
+        
+        conexion.query(sqlProducto, valoresProducto, function (error) {
+            if (error) {
+                console.error(error);
+                return res.status(500).send("Ocurrió un error al actualizar el producto");
+            }
+            console.log("Producto actualizado con éxito");
+            return res.json({ status: "ok", message: "Producto actualizado con éxito" });
+        });
+    }
+    else{
+
+        console.log("entro a no imagen");
+        
+        const sqlProducto = "UPDATE productos SET nom_producto = ?, descripcion = ?, precio = ?, id_categorias = ?, id_talles = ?, cantidad = ? WHERE id_productos = ?";
+        const valoresProducto = [nom_producto, descripcion, precio, id_categorias, id_talles, cantidad, id];
+        
+        conexion.query(sqlProducto, valoresProducto, function (error) {
+            if (error) {
+                console.error(error);
+                return res.status(500).send("Ocurrió un error al actualizar el producto");
+            }
+            console.log("Producto actualizado con éxito");
+            
+            return res.json({ status: "ok", message: "Producto actualizado con éxito" });
+        });
+    }
 });
 
 // Eliminar un producto por ID
